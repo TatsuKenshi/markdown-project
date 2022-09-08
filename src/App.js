a795b19afafdba9b34c2731652fa883f0eabe889
TatsuKenshi <rockinbg@gmail.com>
2022-09-08 23:54:34 +0200

    basic css

--- src/App.js
+++ src/App.js
@@ -1,5 +1,28 @@
+import React, { useState } from "react";
+import ReactMarkdown from "react-markdown";
+
 function App() {
-  return <div className="App"></div>;
+  const [markdown, setMarkdown] = useState("## markdown preview");
+  console.log(markdown);
+
+  return (
+    <>
+      <main>
+        <section>
+          <textarea
+            name=""
+            id=""
+            value={markdown}
+            onChange={(e) => setMarkdown(e.target.value)}
+            className="textinput"
+          ></textarea>
+          <article>
+            <ReactMarkdown>{markdown}</ReactMarkdown>
+          </article>
+        </section>
+      </main>
+    </>
+  );
 }
 
 export default App;
